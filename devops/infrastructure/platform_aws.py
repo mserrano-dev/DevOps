5574b05759e4df5c9ff1ca2a8fbb452fe758261a
ENTslug <Mark.Serrano26@gmail.com>
2018-05-10 12:02:03 -0700

    + build-process: add pip package management via Pipenv. + build-process: add boto3 to Pipfile. + bin/saltmaster: modify new_saltmaster to execute script via pipenv. + devops/infrastructure: implement remove_server for platform_aws.

--- devops/infrastructure/platform_aws.py
+++ devops/infrastructure/platform_aws.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
-import awscli
+import boto3
 from infrastructure.interface import Infrastructure
+import json
 
 # ============================================================================ #
 # Infrastructure via Amazon Web Services
 # ============================================================================ #
 class Platform(Infrastructure):
+    
+    def __init__(self):
+        self.ec2 = boto3.client('ec2')
+        Infrastructure.__init__(self)
+    
     def create_server(self):
         return
     
     def remove_server(self, KEY):
-        print KEY
-        return
+        list_key = [KEY]
+        if isinstance(KEY, 'list') == True:
+            list_key = KEY
+            
+        resp = self.ec2.terminate_instances(InstanceIds=list_key)
+        print json.dumps(resp, indent=4, sort_keys=True, default=str)
+
+        
