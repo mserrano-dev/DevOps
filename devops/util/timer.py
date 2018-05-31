0e91609f49fdb0f7b3ab049137296b95a5fef8f0
ENTslug <Mark.Serrano26@gmail.com>
2018-05-31 13:32:32 -0700

    devops: update timer module to have today and today_w_time functions.

--- devops/util/timer.py
+++ devops/util/timer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import date
+import datetime
 import time
 
 # ============================================================================ #
@@ -9,19 +11,33 @@ def sleep(duration):
     Pause execution for specified duration
     """
     time.sleep(duration)
-    
+
+def today_w_time():
+    """
+    Returns today in format 'Y-m-d H:i:s'
+    """
+    today = datetime.datetime.now()
+    today = today.replace(microsecond=0)
+    return today.isoformat(' ')
+
+def today():
+    """
+    Returns today in format 'Y-m-d'
+    """
+    return date.today()
+
 def time_string(delta, hours, minutes, seconds, delim, always_show=True):
     """
     Convert seconds into the format specified
     """
     t_hours, remainder = divmod(delta, 3600)
     t_minutes, t_seconds = divmod(remainder, 60)
-
+    
     output = []
     if always_show or t_hours > 0: output.append(hours % t_hours)
     if always_show or t_minutes > 0: output.append(minutes % t_minutes)
     if always_show or t_seconds > 0: output.append(seconds % t_seconds)
-
+    
     return delim.join(output)
 
 class Stopwatch():
@@ -30,7 +46,7 @@ class Stopwatch():
     """
     def __init__(self):
         self.__start_time = time.time()
-        
+    
     def output_report(self):
         self.__end_time = time.time()
         
